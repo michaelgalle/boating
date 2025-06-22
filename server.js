5ad141230335b237961be27cc024019be3910c9a
michael.galle@gmail.com <michael.galle@gmail.com>
2025-06-22 12:51:58 -0400

    modified server.js again

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-/*
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const PORT = 3000; // Must run with sudo if using port 80 on Linux
-
-// Serve static files from /public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Optional: fallback route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
-*/
-
 
 const express = require('express');
 const app = express();
